9e05c32c9a18cbb840e0be54b8c868abe9d3247b
schlopp96 <71921821+schlopp96@users.noreply.github.com>
2022-08-01 22:06:36 -0500

    Code Refactors
    ---
    
    - Removed unnessecary `continue` statements.
    - Added additional code comments.
    
    Signed-off-by: schlopp96 <71921821+schlopp96@users.noreply.github.com>

--- VBPatcher/main.py
+++ VBPatcher/main.py
@@ -12,7 +12,7 @@ from PyLoadBar import PyLoadBar
 
 sys.path.insert(0, dirname(
     dirname(__file__)))  # Ensure main module can be found by Python.
-chdir(dirname(__file__))  # Change to main module directory.
+chdir(dirname(__file__))  # Change cwd to main module directory.
 
 import VBPatcher.appglobals.appglobals
 from VBPatcher.applogger.applogger import logger, logger_stream
@@ -21,10 +21,10 @@ from VBPatcher.patching.patching import _Patcher
 from VBPatcher.subprocessing.subprocessing import _exitPatcher, _openValheim
 from VBPatcher.validation.validation import _Validate
 
-DL = _Downloader()
-Patcher = _Patcher()
-Validations = _Validate()
-cancel = PyLoadBar(False)
+DL = _Downloader()  # Initialize Downloader.
+Patcher = _Patcher()  # Initialize Patcher.
+Validations = _Validate()  # Initialize Validations.
+cancel = PyLoadBar(False)  # Initialize text-based loading sequence.
 
 #$ ====================================================================================================== $#
 
@@ -50,54 +50,49 @@ def main() -> None | NoReturn:
             f"Welcome to the Valheim Bepinex Patcher!\n\nPlease Choose an Option by Entering its Corresponding Number:\n\n{VBPatcher.appglobals.appglobals._textborder}\n>> [1] Patch BepInEx to latest stable release: {VBPatcher.appglobals.appglobals.b_stable}\n>> [2] Patch BepInEx to latest development/bleeding-edge build: {VBPatcher.appglobals.appglobals.b_dev}\n>> [3] Apply both patches to BepInEx in chronological order of release ({VBPatcher.appglobals.appglobals.b_stable} then {VBPatcher.appglobals.appglobals.b_dev})\n>> [4] Check for/update to newest patch versions\n>> [5] Open Valheim\n>> [6] Exit Program\n\n> "
         )
 
-        if choosePatch == '1':
+        if choosePatch == '1':  # Patch BepInEx to latest stable release.
             logger.info(
                 'Chose option [1] to patch BepInEx to latest stable release...'
             )
             Patcher._patch_stable()
-            continue
 
-        elif choosePatch == '2':
+        elif choosePatch == '2':  # Patch BepInEx to latest development/bleeding-edge build.
             logger.info(
                 'Chose option [2] to patch BepInEx to latest dev-build...')
             Patcher._patch_dev()
-            continue
 
-        elif choosePatch == '3':
+        elif choosePatch == '3':  # Apply both patches to BepInEx in chronological order of release.
             logger.info(
                 'Chose option [3] to install both stable and development patch builds in order of release...'
             )
             Patcher._patch_full()
-            continue
 
-        elif choosePatch == '4':
+        elif choosePatch == '4':  # Check for/update to newest patch versions.
             logger.info('Chose option [4] to check for new patch updates...')
             DL.UpdatePatcher()
-            continue
 
-        elif choosePatch == '5':
+        elif choosePatch == '5':  # Open Valheim.
             logger.info('Chose option [5] to start Valheim...')
             _openValheim()
+            break
 
-        elif choosePatch == '6':
+        elif choosePatch == '6':  # Exit Program.
             logger.info('Chose option [6] to close patcher...')
             logger.info(
-                'Completed VBPatcher processes...\n>> Preparing to exit...\n'
-            )
+                'Completed VBPatcher processes...\n>> Preparing to exit...\n')
             cancel.start('Completed VBPatcher processes',
                          'Exiting...',
                          iter_total=5,
                          txt_seq_speed=0.25)
             return _exitPatcher()
 
-        else:
+        else:  # Invalid input.
             logger_stream.warning(
                 f'Invalid Input -\n\n>> Your Entry:  "{choosePatch}".\n\n>> Must ONLY enter:\n>> [1] for stable release {VBPatcher.appglobals.appglobals.b_stable}\n>> [2] for development build {VBPatcher.appglobals.appglobals.b_dev}\n>> [3] for FULL upgrade (apply both patches in order of release)\n>> [4] to update available patch versions/builds\n>> [5] to open Valheim\n>> [6] to exit program\n\n'
             )
             sleep(1.5)
-            continue
 
-        return _exitPatcher()
+    return _exitPatcher()
 
 
 if __name__ == '__main__':
